f10ce5d4b78ae3b096fba3b72199b7288d612e7a
310502 <Meet!9975>
2019-12-05 16:56:04 +0530

    [ShivamB]:GEOED-3726:Refund | Technical story : Get ref data from GP2 and refund service and store it in NgRxStore.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import {Store} from "@ngrx/store";
 import {State} from "./reducers";
 import {RefdataService} from "./core/refdata.service";
 import {RefDataAction} from "./actions/refundAction";
+import {combineLatest} from "rxjs";
+import {filter, first, flatMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,15 @@ import {RefDataAction} from "./actions/refundAction";
 export class AppComponent {
 
   constructor(private translateService: TranslateService, private gp2Service: Gp2Service, private authService: AuthService, private store: Store<State>, private refdataService: RefdataService) {
-    refdataService.retrieveRefData().subscribe(value => {
-      let refData = value;
-      this.translateService.setDefaultLang(refData.locales.find(l => l.systemDefault).locale);
-      this.store.dispatch(new RefDataAction(value));
-    });
+    combineLatest([
+      this.gp2Service.token$.pipe(filter(token => !!token), first()),
+      this.gp2Service.apiUri$.pipe(filter(uri => !!uri), first())
+    ]).pipe(flatMap(() => this.refdataService.retrieveRefData()))
+      .subscribe(value => {
+        let refData = value;
+        this.translateService.setDefaultLang(refData.locales.find(l => l.systemDefault).locale);
+        this.store.dispatch(new RefDataAction(value));
+      });
     // window.addEventListener('message', (evt) => { console.log(evt.data); }, false);
   }
 
